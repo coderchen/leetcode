a7a50416e2b24200bd7ad4d5e6bc934d354ea480
coderchen <coderchen@yeah.net>
2014-11-19 10:21:03 +0800

    a

--- CombinationSum2.cpp
+++ CombinationSum2.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private:
-	void solve(vector<vector<int> >& ans, vector<int>& rec, vector<int>& candidates, int target, int idx) {
+	void solve(vector<vector<int> >& ans, vector<int>& rec, 
+			vector<bool>& used, vector<int>& candidates, int target, int idx) {
 		if (target == 0) {
 			ans.push_back(rec);
 			return;
@@ -10,20 +11,19 @@ private:
 			return;
 		}
 		
-		vector<int>::iterator it1 = lower_bound(candidates.begin(), candidates.end(), candidates[idx]);
-		int diff1 = it1 - candidates.begin();
-		int cnt = idx - diff1 + 1;
+		if (idx > 0 && candidates[idx] == candidates[idx-1] && !used[idx-1]) {
+			used[idx] = false;
+			solve(ans, rec, used, candidates, target, idx+1);
+		} else {
+			for (int i = 1; i >= 0; --i)
+				rec.push_back(candidates[idx]);
 		
-		for (int i = 1; i >= 0; --i)
-			rec.push_back(candidates[idx]);
-		
-		for (int i = 1; i >= 0; --i) {
-			rec.pop_back();
-			vector<int>::iterator it2 = lower_bound(rec.begin(), rec.end(), candidates[idx]);
-			int cnt2 = rec.end() - it2;
-			if (cnt > cnt2 && cnt > 1) continue;
-			solve(ans, rec, candidates, target - candidates[idx] * i, idx + 1);
-		}
+			for (int i = 1; i >= 0; --i) {
+				rec.pop_back();
+				used[idx] = i > 0;
+				solve(ans, rec, used, candidates, target - candidates[idx] * i, idx + 1);
+			}
+		}		
 	}
 	
 public:
@@ -31,7 +31,8 @@ public:
         sort(candidates.begin(), candidates.end());
         vector<vector<int> > ans;
         vector<int> record;
-        solve(ans, record, candidates, target, 0);
+		vector<bool> used(candidates.size(), false);
+        solve(ans, record, used, candidates, target, 0);
         return ans;
     }
 };
