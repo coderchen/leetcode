4e86e4759040376dda2ce027e982fd5b1e624eeb
coderchen <coderchen@yeah.net>
2014-12-10 21:23:08 +0800

    FindPeakElement

---
+++ FindPeakElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findPeakElement(const vector<int> &num) {
+		if (num.empty()) return -1;
+		if (num.size() == 1) return 0;
+        if (num[0] > num[1]) return 0;
+		if (num[num.size()-1] > num[num.size()-2]) return num.size()-1;
+		
+		for (int i = 1; i < num.size()-1; ++i)
+			if (num[i] > num[i-1] && num[i] > num[i+1])
+				return i;
+		return -1;
+    }
+};
